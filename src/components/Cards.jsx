353d1c063d4065074228f79b8398250e588f78b6
Miss_Sabrin <sabrincumar008@gmail.com>
2022-08-26 12:14:03 +0300

    first commit

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -16,7 +16,7 @@ const Cards = () => {
                   <p className='py-2 border-b mx-8'>1 Granted User</p>
                   <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
               </div>
-              <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+              <button className='bg-[#df8f2e] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
           </div>
           <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
               <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Double} alt="/" />
@@ -27,7 +27,7 @@ const Cards = () => {
                   <p className='py-2 border-b mx-8'>1 Granted User</p>
                   <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
               </div>
-              <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+              <button className='bg-black text-[#df8f2e] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
           </div>
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
               <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="/" />
@@ -38,7 +38,7 @@ const Cards = () => {
                   <p className='py-2 border-b mx-8'>1 Granted User</p>
                   <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
               </div>
-              <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+              <button className='bg-[#df8f2e] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
           </div>
       </div>
     </div>
